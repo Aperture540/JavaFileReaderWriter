7f359e982fc2bf338b4336074e527b9cab69a324
Anthony Gendron <aperturescience540@gmail.com>
2017-11-21 14:40:03 -0500

    JavaFileReaderWriter

--- src/javafilereaderwriter/JavaFileReaderWriter.java
+++ src/javafilereaderwriter/JavaFileReaderWriter.java
@@ -5,17 +5,49 @@
  */
 package javafilereaderwriter;
 
-/**
- *
- * @author agendron
- */
+import java.io.*;
+import java.util.ArrayList;
+import java.util.*;
+
+
 public class JavaFileReaderWriter {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        
+        try(BufferedReader reader = new BufferedReader(new FileReader("BoyNames.txt"))) {
+            
+            ArrayList<String> uNames = new ArrayList<>();
+            
+            String line = reader.readLine();
+            int counter = 0;
+            
+            while(line != null) {
+                uNames.add(counter, line);
+                line = reader.readLine();
+                counter++;
+            }
+            System.out.println("Read file");
+            
+            Comparator<? super String> c = null;
+            uNames.sort(c);
+            
+            System.out.println("Sorted list");
+            
+            try(BufferedWriter writer = new BufferedWriter(new FileWriter("BoyNamesSorted.txt"))) {
+            for(int i = 0; i < uNames.size(); i++) {
+                writer.append(uNames.get(i));
+                writer.newLine();
+                }
+                writer.close();
+                System.out.println("Wrote list to file");
+            }
+            catch(IOException e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
+        catch(IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
-    
 }
